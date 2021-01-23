815518aa8f51c156d8cdf9aed9ce59bf76afa4c6
Nguyen The Minh <s3878434@rmit.edu.vn>
2021-01-24 00:17:47 +0700

    add

--- js/feedback.js
+++ js/feedback.js
@@ -1,56 +1,142 @@
-var khanhlinh_feedback = ["about herself", "khanhlinh=> ngoctuan", "khanhlinh=> phuong nam", "khanhlinh=> the minh", "khanhlinh=> khai minh"];
-var ngoctuan_feedback = ["ngoctuan => ngoclinh", "about himself", "ngoctuan => phuong nam", "ngoctuan => the minh", "ngoctuan => khai minh"];
-var phuongnam_feedback = ["phuongnam => khanhlinh ", "phuongnam => ngoc tuan", "about himself", "phuongnam=>theminh", "phuongnam => khai minh"];
-var theminh_feedback = ["theminh=> khanhlinh", "theminh=> ngoc tuan", "theminh=> phuong nam", "about himself", "theminh=> khai minh"];
-var khaiminh_feedback = ["khaiminh => khanhlinh ", "khaiminh => ngoctuan", "khaiminh => phuongnam", "khaiminh => theminh", "about himself"];
-var khanhlinh = document.getElementById("khanhlinh");
-var ngoctuan = document.getElementById("ngoctuan");
-var phuongnam = document.getElementById("phuongnam")
-var theminh = document.getElementById("theminh")
-var khaiminh = document.getElementById("khaiminh")
+var khanhlinh_feedback_A = ["I tried my best to help other teammates whenever they need. I contribute my works in both technical and writing parts so I can learn things from my team.  ",
+    "Together, Tuan and Minh manage the team very well. Every part is completed smoothly thanks to them. Individually, Tuan is very responsible with his tasks. He can do so many researches to complete the works to at least meet adequate quality or even further. ",
+    "Complete his works on time. He usually asks our group to revise the report to make sure all the things are in the same path.",
+    "A responsible group leader who always makes sure that all the tasks are completed on time. Working together with other teammates to help them finish their parts.  ",
+    "He and Nam are in charge of writing report. He always did the research before working on the project to bring the good result. "
+];
+var ngoctuan_feedback_A = ["Together, Tuan and Minh manage the team very well. Every part is completed smoothly thanks to them. Individually, Tuan is very responsible with his tasks. He can do so many researches to complete the works to at least meet adequate quality or even further. ",
+    "Did a great job supporting every aspect of the team. Handled the report and presentation smoothly. ",
+    "Great at handling the tasks given. Did a great job in the report and the presentation. ",
+    "Giving out tasks to the rest of the team responsibly. Help the team when in need. Did a great job developing the GitHub website. Motivate teammates to finish their sections on time. ",
+    "Great at handling the tasks given. Did a great job in the report and the presentation. Constantly giving out opinions to members. "
+];
+var phuongnam_feedback_A = ["Participated in every aspect of the group project including writing the report, creating website and making the application. Giving a help to others when they have trouble in their part.",
+    "Project founder. Making demos and also the final product of the application by his knowledge about every corner of the campus. Edit the presentation video and submitted to canvas speedily.",
+    "Finish the task on time with an acceptable quality. King of dividing working task. Make the original presentation slide and transfer to his teammate for the last outcome.    ",
+    "Make a brilliant contribution to the group website. Accomplish his task early and support teammates when they have trouble. ",
+    "Nam’s accompany author of the report and walkthrough it smoothly with the limited about of time and great output. Connected group’s together and communicated actively in the box. "
+];
+var theminh_feedback_A = ["She does not have much confidence in herself, but she is very good with her work. She is ready to accept the work that we give and she even be able to produce a nice final product at the end of it all ",
+    "He works with me very well in organizing the group. When you give him the work and give him enough courage, he will work very diligently.  ",
+    "He tried his best to overcome his weakness. He is very cooperative when working on the work that he is given. ",
+    "I work really hard on trying to communicate with my teammates, giving feedbacks and help them with their works as much as possible so that we can finish this on time ",
+    "He is willing to take in opinions and feedback. Moreover, he always takes in information and research before writing to produce a good result at the end "
+];
+var khaiminh_feedback_A = ["She joined every part of our group and immensely helped us with our work. She finished her task on time and not afraid to ask. She brought to the team joys with her humor.  ",
+    "With the help of Nguyen The Minh, both of them are good leaders. Throughout the semester, they led us to complete the project and help us do our tasks. ",
+    "He did an excellent job on his part, finished his task on time, and helped others with their report. Attend group chat regularly and have the right attitude toward teammates. ",
+    "Finish task on time before deadline with a decent amount of spare time. Have tremendous and consistent opinion when having a discussion. Have a good investment in the project and report. Actively take part in the discussion. ", "I think I have the right attitude overall and did a great job on our report. I still need to improve on my communication and my presentation skill"
+];
+
+var khanhlinh_feedback_O = ["I do not have any knowledge about IT before. Although I am not skilled in any specific part of the project, I do not hesitate to try working on them with my team. I have learnt a lot from my teammates, both technical skills and soft skills. Thank you guys a lot!  ",
+    "He is the app developer. I am amazed that he has made the application so well in such a limited time.  ",
+    "He is the one who in charge with the writing parts. He always tries to complete all the sections of the report as his best. He helps me a lot with my writing parts",
+    "He is skillful in the web development field. He knows how to create a well functioned GitHub repository with HTML, CSS and JavaScript. ",
+    "He has to do lots of writing but he can finish all of them on time. An optimistic teammate that eases the atmosphere when things get serious.  "
+];
+var ngoctuan_feedback_O = ["She knew how to cheer her teammates up when they were stuck in some parts of the project. After this project, her overall skills have improved significantly compare to the start of the project. ",
+    "From the start of the project, I didn’t really know how to do anything except for the report. With the help of the team leader, I was able to create the application. Despite there were still flaws in the making, I still gained experience on how to building an application",
+    "He might not be the best with his words, but he knew how to handle the report and handled it smoothly. Even though he’s not that great on presenting the presentation, in the end, he still did great on his part. ",
+    "Being experienced with GitHub, he managed to create a great website for the project. Furthermore, as the team leader, he knew how to help each teammate according to what they were working on. ",
+    "He knew how to plan out and keep up with the group’s progress. Even when the group stumbled upon a problem on the way, he still kept his positivity and helped the team pull through. "
+];
+var phuongnam_feedback_O = ["She should have more confidence in her work and she did her job really well. She has a part in almost every work inside the project and it is very helpful. ",
+    "Tuan is very active in the project to force us summitting our best work on time with the high fighting spirit. Also, he did a great guiding map for us and newbies.  ",
+    "Learnt a lot after the project and trying to develop my academic skills day by day. Despite criticisms, I am totally confident with my work. However, I am not a good speaker so it took times to finish my presentation part",
+    "Being skillful in using HTML, CSS as well as JavaScript to make an extraordinary group’s website. Also, Minh is a distinguishes leader who encourages us through the project.    ",
+    "Although has undertaken the complicated part of the project, Minh has accomplished his part perfectly before deadline. Also, he has been keeping our group friendliness atmosphere and solve our problems easily.    "
+];
+var theminh_feedback_O = ["She should have more confidence in her work and she did her job really well. She has a part in almost every work inside the project and it is very helpful. ",
+    "He always finishes my request and willing to do more work to produce the best result possible. It is amazing how good he really is when he is given the tasks to do.  ",
+    "He might not be the best at writing report or work on presentation but he is always willing to change his work depend on the feedback which made him a nice teammate to have. ",
+    "I did my best for this project. Although I must admit that there are times that I can do better but by no mean that I am not confident that I did my best. ",
+    "He is really focusing more on preparation before writing the final report. This is a really good trait for a teammate should have to be able to produce the best result. "
+];
+var khaiminh_feedback_O = ["Even though she doubted her confidence, she eventually got through it and did a good job. In the future, I think if she can improve a bit more, she could be an outstanding teammate. ",
+    "He has the right attitude when communicating and willing to help his teammate. He did an excellent job building the application. Besides, I think he can improve his work a little bit more. ",
+    "He is a good worker with a great attitude. Even though he is not a great writer, he did his report pretty well. His communication had improved since the first at the university. ",
+    "Self-learning dramatically improved his technical skills. When dealing with challenging exercises, he has a positive attitude. He was actively asking. soft skills such as interaction have improved. ",
+    "Since it is my first semester, there is a lot to learn, such as my coding and programming skills. In the end, I did what I'm supposed to do and help others with their work. "
+];
+
+var khanhlinh_A = document.getElementById("khanhlinh_about_attitude");
+var ngoctuan_A = document.getElementById("ngoctuan_about_attitude");
+var phuongnam_A = document.getElementById("phuongnam_about_attitude")
+var theminh_A = document.getElementById("theminh_about_attitude")
+var khaiminh_A = document.getElementById("khaiminh_about_attitude")
+var khanhlinh_O = document.getElementById("khanhlinh_about_opinion");
+var ngoctuan_O = document.getElementById("ngoctuan_about_opinion");
+var phuongnam_O = document.getElementById("phuongnam_about_opinion")
+var theminh_O = document.getElementById("theminh_about_opinion")
+var khaiminh_O = document.getElementById("khaiminh_about_opinion")
 
 function getting_feedback(click) {
     erase.innerHTML = ""
     switch (parseInt(click)) {
         //feedback about khanhlinh
         case 1:
-            khanhlinh.innerHTML = khanhlinh_feedback[0]
-            ngoctuan.innerHTML = ngoctuan_feedback[0]
-            phuongnam.innerHTML = phuongnam_feedback[0]
-            theminh.innerHTML = theminh_feedback[0]
-            khaiminh.innerHTML = khaiminh_feedback[0]
+            khanhlinh_A.innerHTML = khanhlinh_feedback_A[0]
+            ngoctuan_A.innerHTML = ngoctuan_feedback_A[0]
+            phuongnam_A.innerHTML = phuongnam_feedback_A[0]
+            theminh_A.innerHTML = theminh_feedback_A[0]
+            khaiminh_A.innerHTML = khaiminh_feedback_A[0]
+            khanhlinh_O.innerHTML = khanhlinh_feedback_O[0]
+            ngoctuan_O.innerHTML = ngoctuan_feedback_O[0]
+            phuongnam_O.innerHTML = phuongnam_feedback_O[0]
+            theminh_O.innerHTML = theminh_feedback_O[0]
+            khaiminh_O.innerHTML = khaiminh_feedback_O[0]
             break;
             //feedback about ngoc tuan
         case 2:
-            khanhlinh.innerHTML = khanhlinh_feedback[1]
-            ngoctuan.innerHTML = ngoctuan_feedback[1]
-            phuongnam.innerHTML = phuongnam_feedback[1]
-            theminh.innerHTML = theminh_feedback[1]
-            khaiminh.innerHTML = khaiminh_feedback[1]
+            khanhlinh_A.innerHTML = khanhlinh_feedback_A[1]
+            ngoctuan_A.innerHTML = ngoctuan_feedback_A[1]
+            phuongnam_A.innerHTML = phuongnam_feedback_A[1]
+            theminh_A.innerHTML = theminh_feedback_A[1]
+            khaiminh_A.innerHTML = khaiminh_feedback_A[1]
+            khanhlinh_O.innerHTML = khanhlinh_feedback_O[1]
+            ngoctuan_O.innerHTML = ngoctuan_feedback_O[1]
+            phuongnam_O.innerHTML = phuongnam_feedback_O[1]
+            theminh_O.innerHTML = theminh_feedback_O[1]
+            khaiminh_O.innerHTML = khaiminh_feedback_O[1]
             break;
             //feedback about Phuong Nam
         case 3:
-            khanhlinh.innerHTML = khanhlinh_feedback[2]
-            ngoctuan.innerHTML = ngoctuan_feedback[2]
-            phuongnam.innerHTML = phuongnam_feedback[2]
-            theminh.innerHTML = theminh_feedback[2]
-            khaiminh.innerHTML = khaiminh_feedback[2]
+            khanhlinh_A.innerHTML = khanhlinh_feedback_A[2]
+            ngoctuan_A.innerHTML = ngoctuan_feedback_A[2]
+            phuongnam_A.innerHTML = phuongnam_feedback_A[2]
+            theminh_A.innerHTML = theminh_feedback_A[2]
+            khaiminh_A.innerHTML = khaiminh_feedback_A[2]
+            khanhlinh_O.innerHTML = khanhlinh_feedback_O[2]
+            ngoctuan_O.innerHTML = ngoctuan_feedback_O[2]
+            phuongnam_O.innerHTML = phuongnam_feedback_O[2]
+            theminh_O.innerHTML = theminh_feedback_O[2]
+            khaiminh_O.innerHTML = khaiminh_feedback_O[2]
             break;
             //feedback about the minh
         case 4:
-            khanhlinh.innerHTML = khanhlinh_feedback[3]
-            ngoctuan.innerHTML = ngoctuan_feedback[3]
-            phuongnam.innerHTML = phuongnam_feedback[3]
-            theminh.innerHTML = theminh_feedback[3]
-            khaiminh.innerHTML = khaiminh_feedback[3]
+            khanhlinh_A.innerHTML = khanhlinh_feedback_A[3]
+            ngoctuan_A.innerHTML = ngoctuan_feedback_A[3]
+            phuongnam_A.innerHTML = phuongnam_feedback_A[3]
+            theminh_A.innerHTML = theminh_feedback_A[3]
+            khaiminh_A.innerHTML = khaiminh_feedback_A[3]
+            khanhlinh_O.innerHTML = khanhlinh_feedback_O[3]
+            ngoctuan_O.innerHTML = ngoctuan_feedback_O[3]
+            phuongnam_O.innerHTML = phuongnam_feedback_O[3]
+            theminh_O.innerHTML = theminh_feedback_O[3]
+            khaiminh_O.innerHTML = khaiminh_feedback_O[3]
             break;
             //feedback about khai minh
         case 5:
-            khanhlinh.innerHTML = khanhlinh_feedback[4]
-            ngoctuan.innerHTML = ngoctuan_feedback[4]
-            phuongnam.innerHTML = phuongnam_feedback[4]
-            theminh.innerHTML = theminh_feedback[4]
-            khaiminh.innerHTML = khaiminh_feedback[4]
+            khanhlinh_A.innerHTML = khanhlinh_feedback_A[4]
+            ngoctuan_A.innerHTML = ngoctuan_feedback_A[4]
+            phuongnam_A.innerHTML = phuongnam_feedback_A[4]
+            theminh_A.innerHTML = theminh_feedback_A[4]
+            khaiminh_A.innerHTML = khaiminh_feedback_A[4]
+            khanhlinh_O.innerHTML = khanhlinh_feedback_O[4]
+            ngoctuan_O.innerHTML = ngoctuan_feedback_O[4]
+            phuongnam_O.innerHTML = phuongnam_feedback_O[4]
+            theminh_O.innerHTML = theminh_feedback_O[4]
+            khaiminh_O.innerHTML = khaiminh_feedback_O[4]
             break;
     }
 }
--- js/skills_and_jobs_javascript.js
+++ js/skills_and_jobs_javascript.js
@@ -1,5 +1,14 @@
-var skill_descrip = ["content 1", "content 2", "content 3", "content 4", "content 5"];
-var job_descrip = ["content 1", "content 2", "content 3", "content 4", "content 5"];
+var job_descrip = ["Every type of groups always seeks a good leader and manager. In preparing, executing, overseeing, managing, and closing programs, project managers take the lead role. They are responsible for the project's whole breadth, project team, finances, and progress or failure. The central part of a project management career is that almost every business field worldwide wants project managers with different specializations, making it a successful career option. Healthcare, aviation, electronics, software development, engineering, architecture, real estate, printing, financial, marketing, manufacturing, education, insurance, government, and many other businesses require and search for successful project manager. ",
+    "In the future, finding an AI developer is a must in order to take the next step. Ai developers help build operating software that can use for robots, systems for artificial intelligence, or other applications for artificial intelligence. To build systems that use artificial intelligence, they may work closely with electrical engineers or robotics engineers, and others. ",
+    "Our project is positively related to electronic devices since we are making a map application. For software programs, an application developer is responsible for designing and changing the source code. The tasks include developing, testing, and programming applications for computers, cell phones, and tablets. These apps aim to support clients with computer tasks or programs. ",
+    "UI developer's job is to make A technology-focused feature that aims to develop digital applications that entice the user seamless human-computer interaction. This aims to enhance the user experience by making interactions with web pages or software quick, enjoyable, easy, and efficient. Almost everywhere, there is a complicated technical framework that communicates with a customer, from the automobile industry to apps to mobile phones, you can find UI developers. "
+]
+var Project_manager = ["- Leadership: As a project manager, you are responsible for having the project finished successfully and leading a team to accomplish that goal. When required, this allows you to inspire and mediate", "- Scheduling: Set up a reasonable timeline and then manage the resources to stay on track so that the project can be completed on time successfully is what a project manager needs to do", "- Communication: Communication goes with leadership like hands and gloves. You can't be a productive leader if you can't explain what you need your team to do", "- Critical thinking: In evaluating and judging a problem or situation, analytical analysis is as rational as you can to form an impartial decision. When you work on a project, you face challenges, and you want your choices to be unbiased. What's better for the project should be the only thing driving the decision. ", "- Patience: Projects require time to be carefully thought about, from analysis to preparation, to run smoothly. This doesn't mean that there won't be challenges. They are always problems. If it's a proposal for improvement or a staff member behaving or clients getting great hopes, it would be infinitely more problematic if you don't have patience."]
+var AI_developer = ["- Adapting to new technology: It's certainly hard to become an AI developer, but how to become an AI engineer if you don't frequently have the motivation and enthusiasm for learning? You must have highly adaptable skills and be a quick learner to be successful in your career.", "- Mathematical skill: To address the complex data, an AI developer should have a level of expertise in mathematics.", "- Fluent in several languages: They must study programming languages such as Python, C++, R, Java, etc., at some stage to become fluent in AI and ML.Python makes building complex algorithms simpler for them.C++ accelerates the entire coding process.", "- Curiosity and creativity: Programmers need to have a curious and innovative attitude to succeed in the AI domain.This helps them find new ways to solve complex issues and come up with creative ideas to break them down.It also helps them to collect abstract knowledge easily and to be clear about the possibilities of processing it well. "]
+var App_developer = ["- Familiarity with platforms: Apart from mobile app development platforms for Android and iOS, it is a big plus to be familiar with API development platforms, cross-platform app development frameworks, and responsive design for mobile web applications.", "- Fluent in several languages: It is crucial to be familiar with most programming languages, especially the popular, basic ones like C, C++, HTML5, JavaScript, etc., to become an Android app developer, iOS app developer, or React Native expert.", "- Business knowledge: Above all, you need to have a clear understanding and a market - centered attitude to the business environment.This will help you develop a mobile app that gains greater exposure and earns higher revenue. "]
+var UI_developer = ["- Interaction design: Interaction design aims to create an excellent experience for the user. That is why most UI disciplines need understanding and practical knowledge of the concepts of interaction design", "- Teamwork and communication skills: Team communication helps to build comprehension among team members and improve productivity.As team members gain the same knowledge of project priorities and goals, work continues efficiently and effectively.This gives us a more effective way to design that fits the work of the team.", "- Mastery in programming languages: Developers of the user interface(UI) incorporate programming, psychology, and innovative design to develop software and hardware intuitive controls.The UI developer's goal is to create an interface that acts as expected by users, enabling a smooth and productive user experience. "]
+var name_of_every_job = ["Project manager", "AI developer", "Application developer", "UI developer"]
+var job_name = document.getElementById("name_of_the_job")
 var job = document.getElementById("job");
 var skill = document.getElementById("skill");
 var erase = document.getElementById("erase")
@@ -9,24 +18,23 @@ function change_job_skill_description(click) {
     switch (parseInt(click)) {
         case 1:
             job.innerHTML = job_descrip[0]
-            skill.innerHTML = skill_descrip[0]
+            skill.innerHTML = Project_manager.join('<br>')
+            job_name = name_of_every_job[0]
             break;
         case 2:
             job.innerHTML = job_descrip[1]
-            skill.innerHTML = skill_descrip[1]
+            skill.innerHTML = AI_developer.join('<br>')
+            job_name = name_of_every_job[1]
             break;
         case 3:
             job.innerHTML = job_descrip[2]
-            skill.innerHTML = skill_descrip[2]
+            skill.innerHTML = App_developer.join('<br>')
+            job_name = name_of_every_job[2]
             break;
         case 4:
             job.innerHTML = job_descrip[3]
-            skill.innerHTML = skill_descrip[3]
-            break;
-        case 5:
-            job.innerHTML = job_descrip[4]
-            skill.innerHTML = skill_descrip[4]
+            skill.innerHTML = UI_developer.join('<br>')
+            job_name = name_of_every_job[3]
             break;
     }
 }
-document.getElementById("testing123").innerHTML = "testing"
